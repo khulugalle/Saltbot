2efa3cdcb6c74258d5b6b6cf4da2f1d3e5d0ed02
Kalinga Hulugalle <kalinga.hulugalle@gmail.com>
2017-08-19 17:15:41 +1000

    Initial setup for logging, project structure and nuget packages

---
+++ SaltBot/SaltBot.ConsoleApp/Startup.cs
@@ -0,0 +1,54 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saltbot.ConsoleApp
+{
+    class Startup
+    {
+        /// <summary>
+        /// The logger for the application
+        /// </summary>
+        private static ILogger _log = LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Saltbot!");
+            SetupLogSource();
+            Console.ReadKey();
+        }
+
+        private static void SetupLogSource()
+        {
+            ConfigurationErrorsException storedException = null;
+            string eventLogSourceName = "Saltbot.ConsoleApp";
+            try
+            {
+                eventLogSourceName = ConfigurationManager.AppSettings["eventLogSourceName"];
+            }
+            catch (ConfigurationErrorsException cee)
+            {
+                storedException = cee;
+            }
+
+            if (!EventLog.SourceExists(eventLogSourceName))
+            {
+                EventLog.CreateEventSource(eventLogSourceName, "Application");
+                if (_log.IsInfoEnabled)
+                {
+                    _log.Info($"Setting up event log source [{eventLogSourceName}] in Application event log.");
+                }
+            }
+
+            if (storedException != null && _log.IsErrorEnabled)
+            {
+                _log.Error(storedException, "Error occurred while attempting to read event log source name from configuration. Source has been configured as default [Saltbot.ConsoleApp]");
+            }
+        }
+    }
+}
